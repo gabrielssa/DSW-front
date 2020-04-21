f4446aac2e655fb96c667b9bb779bb9844fabdf3
Gabriel <gabriel.schubert.araujo@ccc.ufcg.edu.br>
2020-04-21 02:22:49 -0300

    Cadastrar produto funcioando + listar funcionando

--- src/pages/CadastrarProduto/CadastrarProduto.js
+++ src/pages/CadastrarProduto/CadastrarProduto.js
@@ -1,15 +1,88 @@
 import React from 'react';
 import Menu from '../../components/Menu';
+import axios from 'axios';
+import './CadastrarProduto.css'
+import Done from './img/done.png'
+import Error from './img/error.png'
 
 const CadastrarProduto = () => {
+
+    const fileUploadHandler = () => {
+        let target = document.getElementById("file")
+         console.log(target.files[0])
+        let name = document.getElementById("name").value
+        let category = document.getElementById("categoria").value;
+        let amount = document.getElementById("amount").value;
+        let value = document.getElementById("value").value;
+
+        const fd = new FormData();
+
+        if (document.getElementById("file").files.length > 0){
+            fd.append('file', target.files[0])
+        }
+
+        fd.append('name', name)
+        fd.append('category', category)
+        fd.append('amount', amount)
+        fd.append('value', value)
+
+
+
+
+        let appToken = localStorage.getItem('app-token')
+        axios.post('http://localhost:5555/product', fd, {
+            'headers': {
+              'Authorization': 'Bearer ' + appToken
+            }})
+            .then(res => {
+                console.log(res);
+                if(res.status == 200){
+                    if (res.data.err == null){
+                        document.getElementById("form").style.display = "none";
+                        document.getElementById("produtoCriado").style.display = "block";
+                    }else{
+                        document.getElementById("form").style.display = "none";
+                        document.getElementById("erroCriando").style.display = "block";
+                    }
+                }
+            }).catch(function(e) {
+                document.getElementById("form").style.display = "none";
+                document.getElementById("erroCriando").style.display = "block";
+                console.log(e);
+            })
+    }
+
     return (
         <>
             <Menu page="cadastrarProduto"/>
-            <h1>CadastrarProduto</h1>
+            <div id="form">
+                <label for="file" class="form-element" id="imgLabel">Imagem do Produto</label>
+                <input type="file" id="file" class="form-element"/>
 
-            <form>
-            <input type="file"></input>
-            </form>
+                <input type="text" name ="name" id="name" class="form-element" placeholder="Nome do Produto"/>
+                <span id="labelCategoria">Categoria</span>
+                <select id="categoria">
+                    <option value="tv">TVs</option>
+                    <option value="edo">Eletrodomésticos</option>
+                    <option value="vga">Videogames</option>
+                    <option value="cel">Celulares</option>
+                </select>
+
+                <input type="text" name ="amount" id="amount" class="form-element" placeholder="Quantidade em Estoque"/>
+
+                <input type="text" name ="value" id="value" class="form-element" placeholder="Valor do Produto"/>
+                <button onClick={fileUploadHandler} class="form-element" id="btn">Upload</button>
+            </div>
+
+            <div id="produtoCriado">
+                <p>Produto Criado Com Sucesso</p>
+                <img src={Done}></img>
+            </div>
+
+            <div id="erroCriando">
+                <p>Erro na criação do Produto</p>
+                <img src={Error}></img>
+            </div>
         </>
     )
 }
--- src/pages/listarProduto/ListarProduto.js
+++ src/pages/listarProduto/ListarProduto.js
@@ -2,12 +2,63 @@ import React from 'react';
 import Menu from '../../components/Menu';
 import './ListarProduto.css'
 import Api from '../login/Api';
+import axios from 'axios';
 
 const ListarProduto = () => {
 
+    const removeProduct = function(produto){
+        alert('Apagando produto ' + produto.name)
+        appToken = localStorage.getItem('app-token')
+        
+        const config = {
+            'headers': {
+              'Authorization': `Bearer ${appToken}`
+            }}
+        
+        const bodyParameters = {
+           "_id": ""
+        };
+
+        bodyParameters._id = produto._id;
+
+        console.log(bodyParameters);
+        console.log(config);
+        
+        Api.delete( 
+          'http://localhost:5555/product',
+          config,
+        ).then(console.log).catch(console.log);
+
+    }
+
     const listaTvs = () =>{
         let argumento = {
-            "categoria":"tv"
+            "categoria":"tv",
+            "nome":"TVs"
+        }
+        handleRequest(argumento)
+    }
+
+    const listaEdo = () =>{
+        let argumento = {
+            "categoria":"edo",
+            "nome":"Eletrodomésticos"
+        }
+        handleRequest(argumento)
+    }
+
+    const listaVga = () =>{
+        let argumento = {
+            "categoria":"vga",
+            "nome":"Video Games"
+        }
+        handleRequest(argumento)
+    }
+
+    const listaCel = () =>{
+        let argumento = {
+            "categoria":"cel",
+            "nome":"Celulares"
         }
         handleRequest(argumento)
     }
@@ -23,6 +74,15 @@ const ListarProduto = () => {
         .then(res => {
           const test = res.data;
           document.getElementById("produtos").innerHTML = '';
+
+          if (test.length == 0){
+              var result = document.createElement("p")
+              var resultText = document.createTextNode(`Nenhum produto encontrado na categoria de ${values.nome}`);
+              result.appendChild(resultText);
+              document.getElementById("produtos").appendChild(result)
+              
+
+          }
           for (let i = 0; i < test.length; i++){
               var produto = document.createElement("div");
               produto.className = "produto";
@@ -37,8 +97,18 @@ const ListarProduto = () => {
               var quantidadePText = document.createTextNode("Quantidade Disponível: "+test[i].amount)
               quantidadeP.appendChild(quantidadePText);
 
+              var img = document.createElement('img'); 
+              img.src = 'https://i.ibb.co/ncvjf8H/apagar.png';
+              img.id = "img-apagar";
+        
+              img.addEventListener("click", () => {
+                    removeProduct(test[i])
+              })
+                
+
               produto.appendChild(nameP); 
               produto.appendChild(quantidadeP);
+              produto.appendChild(img);
               document.getElementById("produtos").appendChild(produto)
           }
         })
@@ -54,13 +124,14 @@ const ListarProduto = () => {
                 <h1>Categorias</h1>
                 <ul>
                     <li onClick={listaTvs}>TVs</li>
-                    <li>Eletrodomésticos</li>
-                    <li>Videogames</li>
-                    <li>Celulares</li>
+                    <li onClick={listaEdo}>Eletrodomésticos</li>
+                    <li onClick={listaVga}>Videogames</li>
+                    <li onClick={listaCel}>Celulares</li>
                 </ul>
             </aside>
             </div>
             <div id="produtos">
+                <p>Selecione uma categoria</p>
             </div>
         </div>
         </>
