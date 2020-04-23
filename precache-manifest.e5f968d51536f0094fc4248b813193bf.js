5e27928a2925b1933fcabb2806291b7e41bcd0de
Gabriel <gabriel.schubert.araujo@ccc.ufcg.edu.br>
2020-04-23 20:27:19 -0300

    Updates

---
+++ precache-manifest.e5f968d51536f0094fc4248b813193bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fab54cdce0000cd5779ff9c698f6bbba",
+    "url": "/DSW-front/index.html"
+  },
+  {
+    "revision": "2e5d61d57e107e30c383",
+    "url": "/DSW-front/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0fb7449a085368f3a777",
+    "url": "/DSW-front/static/css/main.735bdbb4.chunk.css"
+  },
+  {
+    "revision": "2e5d61d57e107e30c383",
+    "url": "/DSW-front/static/js/2.d2ca105f.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/DSW-front/static/js/2.d2ca105f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb7449a085368f3a777",
+    "url": "/DSW-front/static/js/main.63f4bd29.chunk.js"
+  },
+  {
+    "revision": "538fd3027a1bdb4449f2",
+    "url": "/DSW-front/static/js/runtime-main.8cee81c2.js"
+  }
+]);
