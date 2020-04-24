c1da34aaa535ce5f1f189b4fa6e11c49b7394f5f
Gabriel <gabriel.schubert.araujo@ccc.ufcg.edu.br>
2020-04-23 21:51:59 -0300

    commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DSW-front/precache-manifest.0f49fda5740d052c3ac84ea2ee30fd91.js"
+  "/DSW-front/precache-manifest.e86faf578f10f6ba36800a77173448c2.js"
 );
 
 self.addEventListener('message', (event) => {
