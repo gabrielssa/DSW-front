021c1ebadbbb048f110fcc8aa9c140a39f99a403
Gabriel <gabriel.schubert.araujo@ccc.ufcg.edu.br>
2020-04-25 06:33:36 -0300

    test

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useHistory, Link } from "react-router-dom";
 
+import { useHistory, Link } from "react-router-dom";
 import {ErrorMessage, Formik, Form, Field} from 'formik';
 import * as yup from 'yup';
 import axios from 'axios';
+import GoogleLogin from 'react-google-login';
 
 import Loading from './img/loading.gif';
 import Nuvem from './img/nuvem.png';
@@ -62,11 +63,15 @@ const Login = () => {
 
     }
 
+    const responseGoogle = (response) => {
+        console.log(response);
+    }
+
     return (
         <>
         <div id="login-container">
         <h1>Login</h1>
-        <p>Preencha os campos para logar</p>
+        <p> Preencha os campos para logar</p>
         <p id="feedback"></p>
 
         <Formik initialValues={{}} onSubmit={handleSubmit} validationSchema={validations}>
@@ -106,7 +111,16 @@ const Login = () => {
                 <button className="Login-Btn" type="submit">Login</button>
             </Form>
         </Formik>
-        
+        <div>
+            <h2>Ou faça login pelo google</h2>
+            <GoogleLogin
+                clientId="332676288891-a6r2rr9k2j1ic649c16ie6u5gcu2afs6.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
+        </div>
             <div id="links">
                 <p><Link to="/register">Registrar</Link></p>
                 <p>Não tem uma conta?</p>
