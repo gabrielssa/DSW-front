bca166d2e53020ee45e7ee6d5c32e7568eea8c50
Gabriel <gabriel.schubert.araujo@ccc.ufcg.edu.br>
2020-04-25 16:37:14 -0300

    update

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -26,7 +26,7 @@ const Login = () => {
 
         console.log("Profile obj: "+response.profileObj.name)
 
-        let data = {
+        const data = {
             "name": name,
             "googleId": id
         }
